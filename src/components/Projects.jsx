56a36c8d6334a186fe29ba5657018620593d99cc
Jessica-Maria Freund <jessicamariafreund@gmail.com>
2024-02-04 17:19:10 +0100

    Fix project section design

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,57 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import food from "../images/Steak.svg";
 import art from "../images/Arts.svg";
 import shop from "../images/Ecommerce.svg";
+import ProjectModal from "./ProjectModal";
+import AagMockup from "../images/aagMockup.png";
+import DynerMockup from "../images/dynerMockup.png";
+import LendistaMockup from "../images/lendistaMockup.png";
+import aagLogo from "../images/aacLogo.png";
+import dynerLogo from "../images/dynerLogo.png";
+import lendistaLogo from "../images/lendistaLogo.jpg";
 
 function Projects() {
+  const [popup, setPopup] = useState(false);
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  const projects = [
+    {
+      title: "Animal Art Gallery",
+      shortIntro:
+        "A vibrant social network for art enthusiasts. Upload, like, and comment on art. Create a personalized gallery.",
+      description:
+        "Animal Art Gallery is a dynamic web application and social network designed for art enthusiasts. Users can seamlessly upload their artwork, engage with other artists through likes and comments, and curate their favorite pieces in a personalized gallery.",
+      link: "https://animal-art-gallery.netlify.app",
+      logo: aagLogo,
+      icon: art,
+      mockup: AagMockup,
+      skills: ["React", "NodeJs", "Express", "Typescript"],
+    },
+    {
+      title: "Dyner",
+      shortIntro:
+        "Effortlessly plan meals, save recipes, and organize grocery shopping. Your go-to web app for meal management.",
+
+      description:
+        "Dyner is a dynamic web application designed to revolutionize the way users approach meal planning, recipe organization, and grocery shopping. It offers a seamless and intuitive experience for individuals striving for a more organized and efficient culinary lifestyle.",
+      link: "https://dyner-mealplan.netlify.app",
+      logo: dynerLogo,
+      icon: food,
+      mockup: DynerMockup,
+      skills: ["React", "NodeJs", "Express", "Typescript"],
+    },
+    {
+      title: "Lendista",
+      shortIntro:
+        "Fashion at your fingertips. Explore, rent, and embrace the latest styles with Lendista's e-commerce fashion lending app.",
+      description:
+        "Lendista, an innovative e-commerce fashion lending app, brings a fresh perspective to the world of style. It offers a unique platform where users can effortlessly explore a curated collection of fashionable garments, create memberships, and experience the joy of renting premium clothing. On the administrative front, Lendista provides robust tools for product management, ensuring a seamless and efficient operation.",
+      link: "https://lendista.netlify.app",
+      logo: lendistaLogo,
+      icon: shop,
+      mockup: LendistaMockup,
+      skills: ["React", "NodeJs", "Express", "Typescript"],
+    },
+  ];
+
+  const closeModal = () => {
+    setSelectedProject(null);
+    setPopup(false);
+  };
+
   return (
     <section className="projects" id="projects">
       <h2>My Projects.</h2>
       <div className="cardWrapper">
-        <div className="card">
-          <div className="iconWrapper">
-            <img src={art} alt="art" className="projectIcon" />
+        {projects.map((project, index) => (
+          <div className="card" key={index}>
+            <div className="iconWrapper">
+              <img src={project.icon} alt="art" className="projectIcon" />
+            </div>
+            <h3>{project.title}</h3>
+            <p>{project.shortIntro}</p>
+
+            <Link className="btn" to={project.link}>
+              Visit
+            </Link>
           </div>
-          <h3>Animal Art Gallery</h3>
-          <p>
-            A web application/ social network for users to upload art, like &
-            comment on other users art, and save art in a gallery.
-          </p>
-          <Link className="btn" to="https://animal-art-gallery.netlify.app">
-            Visit
-          </Link>
-        </div>
-        <div className="card">
-          <div className="iconWrapper">
-            {" "}
-            <img src={food} alt="food" className="projectIcon" />
-          </div>
-          <h3>Dyner</h3>
-          <p>
-            A web application for users to create meal plans, save recipes and
-            plan their grocery shopping.
-          </p>
-          <Link className="btn" to="https://dyner-mealplan.netlify.app">
-            Visit
-          </Link>
-        </div>
-        <div className="card">
-          <div className="iconWrapper">
-            {" "}
-            <img src={shop} alt="ecommerce-shop" className="projectIcon" />
-          </div>
-          <h3>Lendista</h3>
-          <p>
-            An e-commerce fashion lending app where users can view clothes and
-            rent them after creating a membership. Whereas
-            admins can create, edit and delete products and approve returned
-            items.
-          </p>
-          <Link className="btn" to="https://lendista.netlify.app">
-            Visit
-          </Link>
-        </div>
+        ))}
+        {popup && (
+          <ProjectModal project={selectedProject} closeModal={closeModal} />
+        )}
       </div>
     </section>
   );
